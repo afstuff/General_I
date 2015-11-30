d900811a4f4b121d6227a337132191ab8327942c
afstuff <afrecruits234>
2015-11-30 12:04:39 +0100

    Please enter the commit message for your changes. Lines starting delete files

---
+++ ABSGeneral.Model/AbsPassTab.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ABSGeneral.Model
+{
+    public class AbsPassTab
+    {
+        public virtual string PWD_GROUP_ID { get; set; }
+        public virtual string PWD_ID { get; set; }
+        public virtual int PWD_REC_NO { get; set; }
+        public virtual string PWD_USER_NAME { get; set; }
+        public virtual string PWD_CODE { get; set; }
+        public virtual string PWD_CODE_RE { get; set; }
+        public virtual string PWD_CODE_CHG { get; set; }
+        public virtual string PWD_CODE_DEL { get; set; }
+        public virtual string PWD_LOGIN_COUNT { get; set; }
+        public virtual string PWD_USER_LEVEL { get; set; }
+        public virtual string PWD_APPROVED_SW { get; set; }
+        public virtual string PWD_LOG_USER { get; set; }
+        public virtual string PWD_CTRL_SW { get; set; }
+        public virtual DateTime? PWD_ALERT_DATE { get; set; }
+        public virtual DateTime? PWD_EFFECTIVE_DATE { get; set; }
+        public virtual DateTime? PWD_EXPIRY_DATE { get; set; }
+        public virtual string PWD_USER_TYPE { get; set; }
+        public virtual string PWD_USER_DEPT { get; set; }
+        public virtual string PWD_USER_GRP { get; set; }
+        public virtual string PWD_ENABLE_SW { get; set; }
+        public virtual string PWD_SETUP_SW { get; set; }
+        public virtual string PWD_SECURITY_SW { get; set; }
+        public virtual string PWD_GENERAL_SW { get; set; }
+        public virtual string PWD_MARKETING_SW { get; set; }
+        public virtual string PWD_POLICY_SW { get; set; }
+        public virtual string PWD_UNDERWRITING_SW { get; set; }
+        public virtual string PWD_CLAIMS_SW { get; set; }
+        public virtual string PWD_REINSURANCE_SW { get; set; }
+        public virtual string PWD_CREDITCONTROL_SW { get; set; }
+        public virtual string PWD_ACCOUNTS_SW { get; set; }
+        public virtual string PWD_FIXEDASSET_SW { get; set; }
+        public virtual string PWD_PAYROLL_SW { get; set; }
+        public virtual string PWD_PERSONNEL_SW { get; set; }
+        public virtual string PWD_ACCOUNTS_SW_GL { get; set; }
+        public virtual string PWD_INVESTMENT_SW { get; set; }
+        public virtual string PWD_PURCHASES_SW { get; set; }
+        public virtual string PWD_INSERT_SW { get; set; }
+        public virtual string PWD_UPDATE_SW { get; set; }
+        public virtual string PWD_DELETE_SW { get; set; }
+        public virtual string PWD_PRINT_SW { get; set; }
+        public virtual string PWD_VIEW_SW { get; set; }
+        public virtual string PWD_PROCESS_SW { get; set; }
+        public virtual string PWD_MGT_CAT_SW { get; set; }
+        public virtual string PWD_MGT_CAT_NUM { get; set; }
+        public virtual string PWD_USER_AUTH { get; set; }
+        public virtual string PWD_STORE_AUTH { get; set; }
+        public virtual string PWD_TEL_NUM { get; set; }
+        public virtual string PWD_TELEXT_NUM { get; set; }
+        public virtual string PWD_FAX_NUM { get; set; }
+        public virtual string PWD_EMAIL_NUM { get; set; }
+        public virtual string PWD_MOBILE_NUM { get; set; }
+        public virtual string PWD_UND_SPC { get; set; }
+        public virtual string PWD_FLAG { get; set; }
+        public virtual string PWD_KEYDTE { get; set; }
+        public virtual string PWD_OPERID { get; set; }
+
+    }
+}
---
+++ ABSGeneral.Web/ForgortPassword.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="ForgortPassword.aspx.cs" Inherits="ABSGeneral.Web.ForgortPassword" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
